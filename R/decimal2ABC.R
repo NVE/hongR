0762dc8d8e9009c34db7f5be118757a098ccd759
Hong Li <honli@uio.no>
2019-11-18 12:00:22 +0100

    decimal2abc
    decimal2abc work on array

--- R/decimal2ABC.R
+++ R/decimal2ABC.R
@@ -1,31 +1,42 @@
 #' decimal2abc
 #' to convert letters to integer 1 -> a, 26 -> z only for lower case
-#' @param num
+#' @param start_num
+#' @param series
 #' @export
-decimal2abc <- function(num) {
-    if ((num <=0) || ((num %% 1) != 0))  {
+#' @example temp_obsnme <- decimal2abc(n_obs, series)
+decimal2abc <- function(start_num, series = series) {
+    if ((start_num <=0) || ((start_num %% 1) != 0))  {
       stop("input must be ingeter larger than 0")
     }
-    ABCstr <- ""
-    ten <- num
+    return_str <- character(length = length(series))
 
-    while (ten != 0 ) {
-        x = ten %% 26
-        if (x == 0) {
-          temp_str <- "Z"
-        } else {
-          temp_str <- intToUtf8(64+x)
-        }
-        ABCstr <- paste(temp_str, ABCstr, sep = "")
-        if (x == 0) {
-          ten <- (ten - 26)/26
-        } else {
-          ten <- (ten - x)/26
+    for (i_s in seq(length(series))) {
+      print(i_s)
+      if (series[i_s] == -9999) {
+        ABCstr <- "!dum!"
+      } else {
+        ten <- start_num + i_s - 1
+        ABCstr <- ""
+        #print(ten)
+        #print(ABCstr)
+        while (ten != 0 ) {
+          x = ten %% 26
+          if (x == 0) {
+            temp_str <- "Z"
+          } else {
+            temp_str <- intToUtf8(64+x)
+          }
+          ABCstr <- paste(temp_str, ABCstr, sep = "")
+          if (x == 0) {
+            ten <- (ten - 26)/26
+          } else {
+            ten <- (ten - x)/26
+          }
         }
 
+      }
+      return_str[i_s] <- tolower(ABCstr)
     }
-
-    return(tolower(ABCstr))
-
+    return(return_str)
 }
 
--- R/swmm_out2ascii.R
+++ R/swmm_out2ascii.R
@@ -5,17 +5,23 @@
 #' @param iType
 #' @param object_name
 #' @param vIndex
+#' @param start_time_str
+#' @param end_time_str
 #' @export
 
-swmm_out2ascii <- function(fileIn, fileOut, iType, object_name, vIndex) {
+swmm_out2ascii <- function(fileIn, fileOut, iType, object_name, vIndex, start_time_str, end_time_str) {
   #fileIn <- "C:\\Users\\honli\\Desktop\\DuringChina\\CatchmentDelineation\\swmm_runs\\pest_calib\\simplest.out"
   #fileOut <- "C:\\Users\\honli\\Desktop\\DuringChina\\CatchmentDelineation\\swmm_runs\\pest_calib\\simplest.txt"
   #iType <- 1
   #object_name <- "161143"
   #vIndex <- 4
   data_out <- read_out(file = fileIn, iType = iType, object_name = object_name, vIndex = vIndex)[[1]][[1]]
-  #time_str <- format(time(data_out), "%Y%m%dT%H%M%S")
-  
+
+  start_time <- as.POSIXct(start_time_str, format = "%Y%m%dT%H%M%S", origin = "1970-01-01")
+  end_time <- as.POSIXct(end_time_str, format = "%Y%m%dT%H%M%S", origin = "1970-01-01")
+
+  data_out <- data_out[which(time(data_out) >= start_time & time(data_out) <= end_time)]
+
   time_str <- format(time(data_out), "%m/%d/%Y %H:%M")
   value_str <- sprintf("%f10", data_out)
   data_frame <- data.frame(time = time_str, value = value_str)
