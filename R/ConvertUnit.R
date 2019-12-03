44ddf087c20badea856d437949d88ad1f45db312
Hong Li <honli@uio.no>
2019-12-03 09:39:24 +0100

    more in Convert unit
    more in Convert unit

--- R/ConvertUnit.R
+++ R/ConvertUnit.R
@@ -2,14 +2,19 @@
 #' @param inN
 #' @param inUnit
 #' @param outUnit
-#' @param areaCat
+#' @param areaCat in m2
 #' @export
-# area are in m2
 ConvertUnit <- function(inN = 1, inUnit = "m3/s", outUnit = "mm/day", areaCat = 1)
 {
   if ((inUnit == "m3/s") && (outUnit == "mm/day")) outN <- inN * 86400 * 1000 / areaCat
+  if ((inUnit == "m3/s") && (outUnit == "mm/5min")) outN <- inN * 300 * 1000 / areaCat
+  if ((inUnit == "m3/s") && (outUnit == "mm/min")) outN <- inN * 60 * 1000 / areaCat
+  if ((inUnit == "m3/s") && (outUnit == "mm/hour")) outN <- inN * 3600 * 1000 / areaCat
+  
+
   if ((inUnit == "m3") && (outUnit == "mm")) outN <- inN * 1000 / areaCat
   if ((inUnit == "mm/day") && (outUnit == "m3/s")) outN <- inN * areaCat / (86400 * 1000)
+  if ((inUnit == "mm/5min") && (outUnit == "m3/s")) outN <- inN * areaCat / (300 * 1000)
   
   if ((inUnit == "kg.m-2.s-1") && (outUnit == "mm/day")) outN <- inN * 86400
   
